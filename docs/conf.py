e9ec33292a6a0a70084c4e5f47e58451d74c33b4
Thib Guicherd-Callin <thib@cs.stanford.edu>
2021-01-06 18:34:43 -0800

    Proper year for 2.0-alpha3.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'LOCKSS System Manual'
-copyright = '2000-2021, LOCKSS Program'
+copyright = '2000-2020, LOCKSS Program'
 author = 'LOCKSS Program'
 
 
