ad3d3e6ade53d15abc2ebcbc215147d4f87a652b
Thib Guicherd-Callin <thib@cs.stanford.edu>
2024-07-29 22:32:22 -0700

    Actually 1.78.3.

--- docs/conf.py
+++ docs/conf.py
@@ -177,6 +177,6 @@ global_substitutions = {
     'K3S_MINOR': '1.21',
     'K3S_PATCH': '1.21.5+k3s1',
     'CLASSIC_MINOR': '1.78',
-    'CLASSIC_PATCH': '1.78.2',
+    'CLASSIC_PATCH': '1.78.3',
 }
 
