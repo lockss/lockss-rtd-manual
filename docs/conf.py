635ca001816474edf8d73f52782f745fdff68554
Thib Guicherd-Callin <thib@cs.stanford.edu>
2024-08-19 17:45:48 -0700

    Cherry-pick conflict resolution error.

--- docs/conf.py
+++ docs/conf.py
@@ -172,7 +172,6 @@ global_substitutions = {
     'LATEST_PATCH': '2.0.90-beta2 NOT YET RELEASED',
     'PREVIOUS_MINOR': '2.0-beta1',
     'PREVIOUS_PATCH': '2.0.82-beta1',
-release.)
     'K8S_MINOR': '1.21',
     'K8S_PATCH': '1.21.5',
     'K3S_MINOR': '1.21',
