cc514eabfe5b0f5b425b7283622b7ca35a413069
Thib Guicherd-Callin <thib@cs.stanford.edu>
2024-08-19 18:02:30 -0700

    Switch up LOCKSS logo to 2.0-beta2.

--- docs/conf.py
+++ docs/conf.py
@@ -126,7 +126,7 @@ html_css_files = [
 master_doc = 'index'
 
 # See https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_logo
-html_logo = 'images/lockss-2.0-beta1-128x128.png'
+html_logo = 'images/lockss-2.0-beta2-128x128.png'
 
 # See https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {
